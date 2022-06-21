6450e2caafe0aaf3c96c1640edc12e0d99347a67
Snehas25 <65017739+Snehas25@users.noreply.github.com>
2022-06-22 00:27:17 +0530

    Add files via upload

---
+++ ascii_video.py
@@ -0,0 +1,64 @@
+#import required libraries
+from PIL import ImageFont, Image, ImageDraw, ImageOps, ImageFilter
+import numpy as np
+import cv2
+import math
+
+# attach the video to asciify
+video_input ="./data/spiral.mp4"
+
+# the character set used 
+character_list=list(" .'`^\",:;Il!i><~+_-?][}{1)(|\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$")
+interval=(len(character_list))/256
+
+char_width=10
+char_height=10
+scale=0.07
+
+# capturing the video
+vcap=cv2.VideoCapture(video_input)
+
+# font to be used
+Font_use=ImageFont.truetype("./fonts/cour.ttf",15)
+
+def get_characters(i):
+    return character_list[math.floor(interval*i)]
+
+# infinite while loop to be exited when the user presses q 
+while True:
+    _,scan=vcap.read()
+    scan=Image.fromarray(scan)  
+
+    # width and height of frame
+    width,height=scan.size
+    # resizing  
+    scan=scan.resize((int(scale*width),int(scale*height*(char_width/char_height))),Image.NEAREST)
+    # new width and height now
+    width,height=scan.size
+    
+    pixel=scan.load()
+    out_scan=Image.new("RGB",(char_width*width,char_height*height),color=(0,0,0))
+    final=ImageDraw.Draw(out_scan)
+
+    # looping across the whole height and width
+    for i in range(height):
+        for j in range(width):
+            # r,g,b at ith height and jth width
+            r,g,b=pixel[j,i]
+            # weighted method or also called as luminosity method
+            # it weighs red, green, and blue according to their wavelengths
+            k=int(0.299*r+0.587*g+0.114*b)
+            pixel[j,i]=(k,k,k)
+            final.text((char_width*j,char_height*i),get_characters(k),font=Font_use,fill=(r,g,b))
+
+    ascii_video=np.array(out_scan)
+    # if keyboard stroke by user then wait
+    stroke=cv2.waitKey(1)
+    # if stroke is 'q' then exit
+    if stroke == ord("q"):
+        break
+    # displaying the asciified video in a gui window
+    cv2.imshow("ascii_video",ascii_video)
+
+capture.release()
+cv2.destroyAllWindows()
